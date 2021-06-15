fa4648fe1f62847c20f6e9b86874cce55473a906
uzairjanjua56@gmail.com <uzairjanjua56@gmail.com>
2021-06-15 14:57:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -117,119 +117,117 @@
 					<!-- <div class="center_inner">
 					</div> -->
 					<div class="main-div">
-						<div class="container">
-							<div class="row">
-							<div class="col-lg-1 col-md-1 col-sm-1"></div>
-								<div class="col-lg-8 col-md-8 col-sm-8">
-									<div class="table-responsive tbable-width">
-										<table class="table table-striped">
-												<thead>
-												<tr>
-													<th>Datum Orderplatzierung</th>
-													<th>ISIN</th>
-													<th>Asset</th>
-													<th>Handelsplatz</th>
-													<th>Stücke</th>
-													<th>Long/Short</th>
-													<th>Kaufkurs</th>
-													<th>Investitionssumme</th>
-													<th>Umrechnung in Euro</th>
-													<th>Datum Orderausführung </th>
-													<th>Kommentar</th>
-													<th>Assetklasse</th>
-													<th>Sektor</th>
-													<th>Land</th>
-													<th>Währung</th>
-													<th>Aktueller Kurs </th>
-													<th>Aktueller Buchgewinn</th>
+						<div class="row">
+						   <div class="col-lg-1 col-md-1 col-sm-1"></div>
+							<div class="col-lg-8 col-md-8 col-sm-8">
+								<div class="table-responsive tbable-width">
+									<table class="table table-striped">
+											<thead>
+											<tr>
+												<th>Datum Orderplatzierung</th>
+												<th>ISIN</th>
+												<th>Asset</th>
+												<th>Handelsplatz</th>
+												<th>Stücke</th>
+												<th>Long/Short</th>
+												<th>Kaufkurs</th>
+												<th>Investitionssumme</th>
+												<th>Umrechnung in Euro</th>
+												<th>Datum Orderausführung </th>
+												<th>Kommentar</th>
+												<th>Assetklasse</th>
+												<th>Sektor</th>
+												<th>Land</th>
+												<th>Währung</th>
+												<th>Aktueller Kurs </th>
+												<th>Aktueller Buchgewinn</th>
 
-												</tr>
-												</thead>
-												<tbody>
-												<tr>
-													<td>2021-04-05</td>
-													<td>1</td>
-													<td>Asset</td>
-													<td>Long</td>
-													<td>Birlen Trading Center</td>
-													<td>2</td>
-													<td>500 $</td>
-													<td>400 $</td>
-													<td>900 €</td>
-													<td>2021-04-05</td>
-													<td>The best Service</td>
-													<td>Asset Class</td>
-													<td>sector</td>
-													<td>USA</td>
-													<td>Doller</td>
-													<td>5.4</td>
-													<td>100 $</td>
+											</tr>
+											</thead>
+											<tbody>
+											<tr>
+												<td>2021-04-05</td>
+												<td>1</td>
+												<td>Asset</td>
+												<td>Long</td>
+												<td>Birlen Trading Center</td>
+												<td>2</td>
+												<td>500 $</td>
+												<td>400 $</td>
+												<td>900 €</td>
+												<td>2021-04-05</td>
+												<td>The best Service</td>
+												<td>Asset Class</td>
+												<td>sector</td>
+												<td>USA</td>
+												<td>Doller</td>
+												<td>5.4</td>
+												<td>100 $</td>
 
-												</tr>
-												<tr>
-													<td>2021-04-05</td>
-													<td>1</td>
-													<td>Asset</td>
-													<td>Long</td>
-													<td>Birlen Trading Center</td>
-													<td>2</td>
-													<td>500 $</td>
-													<td>400 $</td>
-													<td>900 €</td>
-													<td>2021-04-05</td>
-													<td>The best Service</td>
-													<td>Asset Class</td>
-													<td>sector</td>
-													<td>USA</td>
-													<td>Doller</td>
-													<td>5.4</td>
-													<td>100 $</td>
+											</tr>
+											<tr>
+												<td>2021-04-05</td>
+												<td>1</td>
+												<td>Asset</td>
+												<td>Long</td>
+												<td>Birlen Trading Center</td>
+												<td>2</td>
+												<td>500 $</td>
+												<td>400 $</td>
+												<td>900 €</td>
+												<td>2021-04-05</td>
+												<td>The best Service</td>
+												<td>Asset Class</td>
+												<td>sector</td>
+												<td>USA</td>
+												<td>Doller</td>
+												<td>5.4</td>
+												<td>100 $</td>
 
-												</tr>
-												<tr>
-													<td>2021-04-05</td>
-													<td>1</td>
-													<td>Asset</td>
-													<td>Long</td>
-													<td>Birlen Trading Center</td>
-													<td>2</td>
-													<td>500 $</td>
-													<td>400 $</td>
-													<td>900 €</td>
-													<td>2021-04-05</td>
-													<td>The best Service</td>
-													<td>Asset Class</td>
-													<td>sector</td>
-													<td>USA</td>
-													<td>Doller</td>
-													<td>5.4</td>
-													<td>100 $</td>
+											</tr>
+											<tr>
+												<td>2021-04-05</td>
+												<td>1</td>
+												<td>Asset</td>
+												<td>Long</td>
+												<td>Birlen Trading Center</td>
+												<td>2</td>
+												<td>500 $</td>
+												<td>400 $</td>
+												<td>900 €</td>
+												<td>2021-04-05</td>
+												<td>The best Service</td>
+												<td>Asset Class</td>
+												<td>sector</td>
+												<td>USA</td>
+												<td>Doller</td>
+												<td>5.4</td>
+												<td>100 $</td>
 
-												</tr>
-												</tbody>
-										</table>
-									</div>	
-								</div>
-								<div class="col-lg-2 col-md-2 col-sm-2 teaserbox">
-									<h4>TeaserBox</h4>
-									<div class="teaserbox-div">
-										<h3>Mentor Programm</h3>
-										<div>
-											<p>unser nachstes 6-monatiges Mentor-Programm</p>
-										</div>
-										<div>
-											<p>Seminar vom</p>
-											<p>03. -5.September 2021 in Munchen</p>
-										</div>
-										<div>
-											<p>Seminar vom</p>
-											<p>12. -14.November 2021 in Munchen</p>
-										</div>
+											</tr>
+											</tbody>
+									</table>
+								</div>	
+							</div>
+						    <div class="col-lg-2 col-md-2 col-sm-2 teaserbox">
+								<h4>TeaserBox</h4>
+								<div class="teaserbox-div">
+									<h3>Mentor Programm</h3>
+									<div>
+										<p>unser nachstes 6-monatiges Mentor-Programm</p>
+									</div>
+								    <div>
+										<p>Seminar vom</p>
+										<p>03. -5.September 2021 in Munchen</p>
+									</div>
+									<div>
+										<p>Seminar vom</p>
+										<p>12. -14.November 2021 in Munchen</p>
 									</div>
 								</div>
-								<div class="col-lg-1 col-md-1 col-sm-1"></div>
 							</div>
-						</div>
+							<div class="col-lg-1 col-md-1 col-sm-1"></div>
+					    </div>
 					 </div>	
 
 						
@@ -249,173 +247,111 @@
 			</div>
 
 		</div><!-- end row push -->
+	
+</div>
 
-		<!-- <footer class="row">
+	<footer class="page-footer font-small blue-grey lighten-5">
 
-			<div class="center">
+		<div style="background-color: #21d192;">
+			<div class="container">
 
-				<div class="center_inner">
+			<!-- Grid row-->
+			<!-- <div class="row py-4 d-flex align-items-center">
 
-					<div class="footer_top clearfix">
+				<!-- Grid column -->
+				
+				<!-- Grid column -->
 
-						<a href="./" title="zur Startseite" class="logo_footer"><img src="include/images/logo.png" width="129" height="53" alt="FORMATIONSTRADER Logo"></a>
+				<!-- Grid column -->
+				<div class="col-md-6 col-lg-7 text-center text-md-right">
 
-						<nav class="social_nav clearfix">
-
-							<ul>
-
-								<li><a href="mailto:kontakt@formationstrader.de" class="mail no_transit" title="E-Mail an Formationstrader"></a></li>
-
-								<li><a href="http://www.facebook.com/share.php?u={{env::host}}/{{env::request}}&amp;t=Check out this site" class="facebook no_transit" title="Share this on facebook" target="_blank"></a></li>
-
-								<li><a href="http://twitter.com/home?status=Check out this site:{{env::host}}/{{env::request}}" class="twitter no_transit" title="Tweet this" target="_blank"></a></li>
-
-								<li><a href="https://plus.google.com/share?url={{env::host}}/{{env::request}}" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="google no_transit" title="Share on Google+"></a></li>
-
-							</ul>
-
-						</nav>
-
-					</div>
-
-					<div class="footer_bottom clearfix">
-
-						<div class="footer_nav_1">
-
-					
-
-						</div>
-
-						<div class="footer_nav_2">
-
-							
-
-						</div>						
-
-						<div class="footer_nav_4">
-
-							<p>Copyright &copy; {{date::Y}} Formationstrader<br>All rights reserved.<span class="break"><br>&nbsp;</span><br><a href="impressum.html" class="orange">Impressum</a></p>
-
-						</div>
-
-						<div class="footer_nav_3">
-
-							<p>Kontakt<br>Dr. Hamed Esnaashari<br><a href="mailto:kontakt@formationstrader.de" class="orange">kontakt@formationstrader.de</a></p>
-
-						</div>
-
-					</div>
+				<!-- Facebook -->
+				<a class="fb-ic">
+					<i class="fab fa-facebook-f white-text mr-4"> </i>
+				</a>
+				<!-- Twitter -->
+				<a class="tw-ic">
+					<i class="fab fa-twitter white-text mr-4"> </i>
+				</a>
+				<!-- Google +-->
+				<a class="gplus-ic">
+					<i class="fab fa-google-plus-g white-text mr-4"> </i>
+				</a>
+				<!--Linkedin -->
+				<a class="li-ic">
+					<i class="fab fa-linkedin-in white-text mr-4"> </i>
+				</a>
+				<!--Instagram-->
+				<a class="ins-ic">
+					<i class="fab fa-instagram white-text"> </i>
+				</a>
 
 				</div>
+				<!-- Grid column -->
+
+			</div> 
+			<!-- Grid row-->
 
 			</div>
+		</div>
 
-		</footer> -->
-<!-- Footer -->
-<!-- Footer -->
-<!-- Footer -->
-<footer class="page-footer font-small blue-grey lighten-5">
+	<!-- Footer Links -->
+		<div class="container text-center text-md-left mt-5">
 
-  <div style="background-color: #21d192;">
-    <div class="container">
+			<!-- Grid row -->
+			<div class="row mt-3 dark-grey-text">
 
-      <!-- Grid row-->
-      <!-- <div class="row py-4 d-flex align-items-center">
+			<!-- Grid column -->
+			<div class="col-md-3 col-lg-4 col-xl-3 mb-4">
 
-        <!-- Grid column -->
-        
-        <!-- Grid column -->
+				<!-- Content -->
+				<h6 class="text-uppercase font-weight-bold">Company name</h6>
+				<hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+				<p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+				consectetur
+				adipisicing elit.</p>
 
-        <!-- Grid column -->
-        <div class="col-md-6 col-lg-7 text-center text-md-right">
+			</div>
+			<!-- Grid column -->
 
-          <!-- Facebook -->
-          <a class="fb-ic">
-            <i class="fab fa-facebook-f white-text mr-4"> </i>
-          </a>
-          <!-- Twitter -->
-          <a class="tw-ic">
-            <i class="fab fa-twitter white-text mr-4"> </i>
-          </a>
-          <!-- Google +-->
-          <a class="gplus-ic">
-            <i class="fab fa-google-plus-g white-text mr-4"> </i>
-          </a>
-          <!--Linkedin -->
-          <a class="li-ic">
-            <i class="fab fa-linkedin-in white-text mr-4"> </i>
-          </a>
-          <!--Instagram-->
-          <a class="ins-ic">
-            <i class="fab fa-instagram white-text"> </i>
-          </a>
+			<!-- Grid column -->
 
-        </div>
-        <!-- Grid column -->
+				
 
-      </div> 
-      <!-- Grid row-->
+				
 
-    </div>
-  </div>
+			<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4"> 
 
-  <!-- Footer Links -->
-  <div class="container text-center text-md-left mt-5">
+				<!-- Links -->
+				<h6 class="text-uppercase font-weight-bold">Contact</h6>
+				<hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+				<p>
+				<i class="fas fa-home mr-3"></i> Dr. Hamed Esnaashari</p>
+				<p>
+				<i class="fas fa-envelope mr-3"></i> kontakt@formationstrader.de</p>
+			
 
-    <!-- Grid row -->
-    <div class="row mt-3 dark-grey-text">
+			</div>
+			<!-- Grid column -->
 
-      <!-- Grid column -->
-      <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+			</div>
+			<!-- Grid row -->
 
-        <!-- Content -->
-        <h6 class="text-uppercase font-weight-bold">Company name</h6>
-        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-          consectetur
-          adipisicing elit.</p>
+		</div>
+	<!-- Footer Links -->
 
-      </div>
-      <!-- Grid column -->
+	<!-- Copyright -->
+		<div class="footer-copyright text-center text-black-50 py-3">© 2020 Copyright:
+			<a class="dark-grey-text" href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+		</div>
+	<!-- Copyright -->
 
-      <!-- Grid column -->
-
-        
-
-        
-
-       <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4"> 
-
-        <!-- Links -->
-        <h6 class="text-uppercase font-weight-bold">Contact</h6>
-        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <i class="fas fa-home mr-3"></i> Dr. Hamed Esnaashari</p>
-        <p>
-          <i class="fas fa-envelope mr-3"></i> kontakt@formationstrader.de</p>
-     
-
-      </div>
-      <!-- Grid column -->
-
-    </div>
-    <!-- Grid row -->
-
-  </div>
-  <!-- Footer Links -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center text-black-50 py-3">© 2020 Copyright:
-    <a class="dark-grey-text" href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
+	</footer>
 <!-- Footer -->
 <!-- Footer -->
 
 <!-- Footer -->
-	</div>
+</div>
 
 	
 
